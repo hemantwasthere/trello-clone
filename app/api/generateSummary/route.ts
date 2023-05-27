da40b01e08e808a210dd036ed3a328d10a9b8cee
Hemant <hrsmart2150@gmail.com>
2023-05-27 12:12:40 +0530

    openAI api endpoint done

---
+++ app/api/generateSummary/route.ts
@@ -0,0 +1,32 @@
+import openai from "@/openai"
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    // todos in the body of the POST request
+    const { todos } = await request.json()
+
+    // communicate with openAI GPT
+    const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        temperature: 0.8,
+        n: 1,
+        stream: false,
+        messages: [
+            {
+                role: 'system',
+                content: `When responding, welcome the user always as Mr. Hemant and say welcome to the our Trello Todo App! Limit the response to 200 characters`
+            },
+            {
+                role: 'user',
+                content: `Hi there, provide a summary of the following todos. Count how many todos are in each category such as To do, in progress and done, then tell the user to have a productive day! Here's the data: ${JSON.stringify(todos)}}`
+            },
+        ]
+    })
+
+    const { data } = response
+
+    console.log("DATA IS: ", data);
+    console.log(data.choices[0].message)
+
+    return NextResponse.json(data.choices[0].message)
+}
