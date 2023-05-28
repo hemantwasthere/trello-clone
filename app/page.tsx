08995568960d002c20d0db41b3d6c596195ba253
Hemant <hrsmart2150@gmail.com>
2023-05-29 00:48:58 +0530

    Removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { account } from "@/appwrite";
 import Board from "@/components/Board";
 import Header from "@/components/Header";
-import SignIn from "@/components/SignIn";
-import { AppwriteException } from "appwrite";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
